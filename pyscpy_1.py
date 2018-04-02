2e5f56006aaf6cb60024725a98a214627d732d86
wanghao1985 <glory_wang@163.com>
2018-04-02 22:23:39 +0800

    Update pyscpy_1.py
    补充对python2 和 python3 的 urllib库区分

--- pyscpy_1.py
+++ pyscpy_1.py
@@ -9,3 +9,12 @@ import urllib.request
 response = urllib.request.urlopen("http://www.baidu.com")
 print(response.read())
 
+
+ #   在Pytho2.x中使用import urllib2——-对应的，在Python3.x中会使用import urllib.request，urllib.error。
+ #   在Pytho2.x中使用import urllib——-对应的，在Python3.x中会使用import urllib.request，urllib.error，urllib.parse。
+ #   在Pytho2.x中使用import urlparse——-对应的，在Python3.x中会使用import urllib.parse。
+ #   在Pytho2.x中使用import urlopen——-对应的，在Python3.x中会使用import urllib.request.urlopen。
+ #   在Pytho2.x中使用import urlencode——-对应的，在Python3.x中会使用import urllib.parse.urlencode。
+ #   在Pytho2.x中使用import urllib.quote——-对应的，在Python3.x中会使用import urllib.request.quote。
+ #   在Pytho2.x中使用cookielib.CookieJar——-对应的，在Python3.x中会使用http.CookieJar。
+ #   在Pytho2.x中使用urllib2.Request——-对应的，在Python3.x中会使用urllib.request.Request。
